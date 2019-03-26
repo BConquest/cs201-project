dcdb56f382745f8d637c26cf469eb5773b6c708d
BConquest <bryant.conquest@gmail.com>
2019-03-26 14:41:46 -0500

    Stuffchanged

--- include/config.h
+++ include/config.h
@@ -1,5 +1,6 @@
+/* must be alpha-numeric 1 leter no escape sequences*/
 const char player1 = 'O';
 const char player2 = 'X';
 
 /* Set if screen should be cleared(1) or not (0) values other than this will break stuff*/
-const int DEBUG = 0;
+const int DEBUG = 1;
